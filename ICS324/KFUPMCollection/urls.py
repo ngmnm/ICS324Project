69f4f43862a63b987e1964f5422d564e386ed389
Nawaf Alfaifi <nowaf142011@hotmail.com>
2020-12-04 17:36:20 +0300

    adding pics to the design

--- ICS324/KFUPMCollection/urls.py
+++ ICS324/KFUPMCollection/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='KFUPMCollection/home.html'),
-    path('about/', views.about, name='blog-about'),
+    path('about/', views.about, name='KFUPMCollection/about.html'),
 ]
--- ICS324/KFUPMCollection/views.py
+++ ICS324/KFUPMCollection/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import dep
 
 
 def home(request):
-    
-    return render(request, 'KFUPMCollection/home.html')
+    context = {
+        'department': dep.objects.all()
+    }
+    return render(request, 'KFUPMCollection/home.html', context )
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'KFUPMCollection/about.html')
