2c0d36fe43758c54a4db0d0d7d685487f29225c9
Nawaf Alfaifi <nowaf142011@hotmail.com>
2020-12-08 08:24:58 +0300

    user registeration

---
+++ ICS324/users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created from {username}!')
+            return redirect('KFUPMCollection/home.html')
+
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/signup.html', {'form': form})
